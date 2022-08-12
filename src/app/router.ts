eca1d0c1ef5111f7920f76de40000043f778c555
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-08-12 11:01:40 +0100

    testing prettier

--- src/app/router.ts
+++ src/app/router.ts
@@ -3,10 +3,11 @@ import { Router, Request, Response, NextFunction } from 'express';
 export const router = Router();
 function dispatchResponse(scope: string) {
   return (req: Request, res: Response): void => {
-    res.status(res.statusCode ?? 200).send({
+                res.status(res.statusCode ?? 200).send({
       success: { ...res.locals[scope] },
     });
   };
 }
+
 
 

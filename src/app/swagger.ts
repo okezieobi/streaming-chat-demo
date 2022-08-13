f59face26052d61183b803ba7532dbe2abb571d7
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-08-13 14:14:11 +0100

    saving progress

--- src/app/swagger.ts
+++ src/app/swagger.ts
@@ -1,23 +1,23 @@
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUI from 'swagger-ui-express';
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 
 const server = new Map();
-server.set('production', {
-  url: '.herokuapp.com/api/v1',
-  description: 'Deployed production ready app app on Heroku',
+server.set("production", {
+  url: ".herokuapp.com/api/v1",
+  description: "Deployed production ready app app on Heroku",
 });
 
-server.set('development', {
-  url: 'http://localhost:5000/api/v1',
-  description: 'Local development/testing app',
+server.set("development", {
+  url: "http://localhost:5000/api/v1",
+  description: "Local development/testing app",
 });
 
 const swaggerDefinition = {
-  openapi: '3.0.3',
+  openapi: "3.0.3",
   info: {
-    title: 'Simple streaming chat REST API', // Title of the documentation
-    version: '1.0.0', // Version of the app
-    description: 'REST API for a stream chat api demo app', // short description of the app
+    title: "Simple streaming chat TEST REST API", // Title of the documentation
+    version: "1.0.0", // Version of the app
+    description: "REST API for a stream chat api demo app", // short description of the app
   },
   servers: [server.get(process.env.NODE_ENV)],
   components: {},
@@ -28,7 +28,7 @@ const options = {
   // import swaggerDefinitions
   swaggerDefinition,
   // path to the API docs
-  apis: ['./docs/**/*.yml'],
+  apis: ["./docs/**/*.yml"],
 };
 // initialize swagger-jsdoc
 export default {
